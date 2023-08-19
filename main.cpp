1d7083064a28b25eb68b94b485561f4c8dacd423
sledge79 <tonywalsh1511@gmail.com>
2023-08-19 10:58:14 +0000

    Added digital outs instead of pwm
    Signed-off-by: sledge79 <tonywalsh1511@gmail.com>

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@
 static DevI2C devI2c(PB_11, PB_10);
 static LSM6DSLSensor acc_gyro(&devI2c, 0xD4, D4, D5); 
 
-PwmOut redLed(PE_3);
-PwmOut greenLed(PB_14);
-PwmOut blueLed(PC_9);
+DigitalOut redLed(PE_3);
+DigitalOut greenLed(PA_5);
+DigitalOut blueLed(PC_9);
 
 float compute(int x, int y, int z, int x_ref, int y_ref, int z_ref) {
     float dot_product = x * x_ref + y * y_ref + z * z_ref;
@@ -30,28 +30,25 @@ int main() {
     acc_gyro.enable_x();
     acc_gyro.enable_g();
 
-    redLed.period_ms(1);
-    greenLed.period_ms(1);
-    blueLed.period_ms(1);
-
-    
-
     while (1) {
-
         acc_gyro.get_x_axes(axes);
         res = compute(axes[0], axes[1], axes[2], 1, 0, 0);  
 
-        redLed = (90.0 - res) / 90.0;  
+        // Turn on the LED corresponding to the x-axis orientation
+        redLed = (res < 45.0);
+
         acc_gyro.get_x_axes(axes);
         res = compute(axes[0], axes[1], axes[2], 0, 1, 0);  
-        
-        greenLed = (90.0 - res) / 90.0;  
+
+        // Turn on the LED corresponding to the y-axis orientation
+        greenLed = (res < 45.0);
+
         acc_gyro.get_x_axes(axes);
         res = compute(axes[0], axes[1], axes[2], 0, 0, 1); 
 
-        
-        blueLed = (90.0 - res) / 90.0;  
+        // Turn on the LED corresponding to the z-axis orientation
+        blueLed = (res < 45.0);
 
-        
+        wait_us(200);
     }
 }
